8a1b1a0cb99b6e9d2ccbbe3646cc8378e83ba33f
Adam Heald <adamheald13@gmail.com>
2018-12-01 23:46:12 -0600

    Add html loader and create template html

--- src/client-app/webpack.config.js
+++ src/client-app/webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './entries/app/index.js',
     footer: './entries/footer/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'WoW Buddy',
+      template: 'index.html'
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
